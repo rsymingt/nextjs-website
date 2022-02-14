bd2b06b04c5c2bf1c2eb620deb48559741d0a3b3
Ryan <ryanwsymington@gmail.com>
2022-02-13 21:35:12 -0500

    update navbar mobile styling

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -40,7 +40,7 @@ export default function Navbar({ navigation, onNavClick, current }: {
                 <div className="relative flex items-center justify-between h-16">
                     <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                     {/* Mobile menu button*/}
-                    <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                    <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-neutral-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                         <span className="sr-only">Open main menu</span>
                         {open ? (
                         <XIcon className="block h-6 w-6" aria-hidden="true" />
@@ -51,21 +51,6 @@ export default function Navbar({ navigation, onNavClick, current }: {
                     </div>
                     <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                     <div className="flex-shrink-0 flex items-center">
-                        {/* <img
-                        className="block invisible h-8 w-auto"
-                        src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
-                        alt="Workflow"
-                        /> */}
-                        {/* <img
-                        className="block lg:hidden h-8 w-auto"
-                        src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
-                        alt="Workflow"
-                        /> */}
-                        {/* <img
-                        className="hidden lg:block h-8 w-auto"
-                        src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
-                        alt="Workflow"
-                        /> */}
                     </div>
                     <div className="hidden sm:block sm:ml-6">
                         <div className="flex space-x-4">
@@ -192,14 +177,14 @@ export default function Navbar({ navigation, onNavClick, current }: {
                 <div className="px-2 pt-2 pb-3 space-y-1">
                     {navigation.map((item, i) => (
                     <Disclosure.Button
-                        key={item.name}
-                        as="a"
-                        href={item.href}
-                        className={classNames(
-                            i === current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                        'block px-3 py-2 rounded-md text-base font-medium'
-                        )}
-                        aria-current={i === current ? 'page' : undefined}
+                    key={item.name}
+                    as="a"
+                    href={item.href}
+                    className={classNames(
+                        i === current ? 'bg-vibrant-red text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    'block px-3 py-2 rounded-md text-base font-medium'
+                    )}
+                    aria-current={i === current ? 'page' : undefined}
                     >
                         {item.name}
                     </Disclosure.Button>
